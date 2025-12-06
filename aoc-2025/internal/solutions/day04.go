fadcb4ff425e99e87be602c03667e4a32d3ce53f
Martin Hodde <martinhodde271@gmail.com>
2025-12-06 15:44:58 -0800

    2025 day 4 part 2

--- aoc-2025/internal/solutions/day04.go
+++ aoc-2025/internal/solutions/day04.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 )
 
-// Symbol representing a paper roll in the grid
+// Symbols that appear in the grid
 const paperRoll = '@'
+const emptySpace = '.'
 
 // Directions for 8 neighboring cells
 var directions = [8][2]int{
@@ -22,41 +23,66 @@ func init() {
 
 func SolveDay4Part1(input []string) string {
 	grid := ParseGrid(input)
-	numPaperRolls := NumPaperRollsAccessibleByForklift(grid)
+	numPaperRolls := len(PaperRollsAccessibleByForklift(grid))
 	return fmt.Sprintf("The number of paper rolls accessible by forklift is %d", numPaperRolls)
 }
 
 func SolveDay4Part2(input []string) string {
-	return ""
+	grid := ParseGrid(input)
+	numPaperRolls := NumPaperRollsRemoved(grid)
+	return fmt.Sprintf("The total number of paper rolls removed by the forklift is %d", numPaperRolls)
 }
 
-// NumPaperRollsAccessibleByForklift counts the number of paper rolls in the grid
-// that can be accessed by a forklift.
-func NumPaperRollsAccessibleByForklift(grid [][]rune) int {
-	count := 0
-	for y := range len(grid) {
-		for x := range len(grid[0]) {
-			if grid[y][x] == paperRoll && IsAccessibleByForklift(grid, x, y) {
-				count++
+// NumPaperRollsRemoved calculates the total number of paper rolls that can be removed
+// from the grid by repeatedly removing accessible paper rolls until none remain.
+func NumPaperRollsRemoved(grid [][]rune) int {
+	removedCount := 0
+	paperRolls := PaperRollsAccessibleByForklift(grid)
+	for len(paperRolls) > 0 {
+		removedCount += len(paperRolls)
+		RemovePaperRolls(grid, paperRolls)
+		paperRolls = PaperRollsAccessibleByForklift(grid)
+	}
+
+	return removedCount
+}
+
+// RemovePaperRolls removes the paper rolls at the specified locations
+// from the grid by marking them as empty.
+func RemovePaperRolls(grid [][]rune, locations [][2]int) {
+	for _, loc := range locations {
+		x, y := loc[0], loc[1]
+		grid[x][y] = emptySpace
+	}
+}
+
+// PaperRollsAccessibleByForklift computes the coordinates of the paper rolls
+// in the grid that can be accessed by a forklift.
+func PaperRollsAccessibleByForklift(grid [][]rune) [][2]int {
+	locations := [][2]int{}
+	for x := range len(grid) {
+		for y := range len(grid[0]) {
+			if grid[x][y] == paperRoll && IsAccessibleByForklift(grid, x, y) {
+				locations = append(locations, [2]int{x, y})
 			}
 		}
 	}
 
-	return count
+	return locations
 }
 
 // IsAccessibleByForklift checks if a paper roll at position (x, y) can be accessed by a forklift
 // based on the surrounding paper rolls. If there are 4 or more adjacent paper rolls, it is not accessible.
 func IsAccessibleByForklift(grid [][]rune, x, y int) bool {
 	rows, cols := len(grid), len(grid[0])
-	if x < 0 || x >= cols || y < 0 || y >= rows {
+	if x < 0 || x >= rows || y < 0 || y >= cols {
 		return false
 	}
 
 	paperRollCount := 0
 	for _, dir := range directions {
 		nx, ny := x+dir[0], y+dir[1]
-		if nx >= 0 && nx < cols && ny >= 0 && ny < rows && grid[ny][nx] == paperRoll {
+		if nx >= 0 && nx < rows && ny >= 0 && ny < cols && grid[nx][ny] == paperRoll {
 			paperRollCount++
 			if paperRollCount >= 4 {
 				return false
